697cd7dec1f1012138cde021f9d87c7fe05ca243
Jovan <jovanfoster9@gmail.com>
2020-06-12 21:23:05 -0400

    added ability to mark a position

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,36 @@
-positions = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+$positions = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-game_board = [
-    [" #{positions[0]} | #{positions[1]} | #{positions[2]} "], 
-    ["---+---+---"], 
-    [" #{positions[3]} | #{positions[4]} | #{positions[5]} "], 
-    ["---+---+---"], 
-    [" #{positions[6]} | #{positions[7]} | #{positions[8]} "]]
-  
-def game(game_board)
-    game_over = false
-    while !game_over do
-        puts game_board 
-         
+def draw_board()
+    puts " #{$positions[0]} | #{$positions[1]} | #{$positions[2]} \n" + 
+    "---+---+---\n" + 
+    " #{$positions[3]} | #{$positions[4]} | #{$positions[5]} \n" + 
+    "---+---+---\n" +
+    " #{$positions[6]} | #{$positions[7]} | #{$positions[8]} \n"
+end
+
+class Player
+    def initialize(marker)
+        @marker = marker
+    end
+
+    def take_turn()
+        puts "Enter a position to place a #{@marker}: "
+        pos = gets.chomp.to_i
+        $positions[pos] = @marker
     end
 end
 
-game(game_board)
+
+def game()
+    player1 = Player.new('x')
+    player2 = Player.new('o')
+    game_over = false
+    while !game_over do
+        draw_board() 
+        player1.take_turn() 
+        draw_board()
+        game_over = true
+    end
+end
+
+game()
